d146d21989cf83a6d30767b36be24e742e25283c
tiagobpires <tiagobarrospires@gmail.com>
2021-08-24 20:47:56 -0300

    Add jinja in more about you section

--- application.py
+++ application.py
@@ -10,5 +10,19 @@ def index():
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+
+    more_about_you = {
+        '🔭 I’m currently working on' : 'Be a good student :P',
+        '🎓 Graduated in ' : '',
+        '📫 Reach me ' : 'tiagobarrospires@gmail.com',
+        "💡 I'm interested about " : 'Computer Science, web development, games and series',
+        '👨‍💻 See my portfolio at ' : 'www.portfolio.com',
+        '💬 Ask me about ' : 'Competitive programming',
+        '🌱 Learning about ' : 'Web development and Competitive programming',
+        '🤝 I like to collaborate in ' : 'Open Source projects',
+        '😄 Pronouns ' : 'He/His',
+        '⚡ Fun fact ' : ':0'
+    }
+
+    return render_template('profile.html', more_about_you=more_about_you)
     
